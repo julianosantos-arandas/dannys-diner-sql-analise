bc46e2673fe070fe189e38977b4d28d66455e832
Juliano Santos <arandasjuliano@gmail.com>
2026-04-29 12:29:06 -0300

    feat: adiciona queries 1 a 3 da análise do Danny's Diner

---
+++ sql/analysis_query.sql
@@ -0,0 +1,61 @@
+=================================================================
+1 - Qual foi o valor total gasto por cada cliente no restaurante?
+=================================================================
+
+SELECT 
+    v.cliente_id,
+    SUM(m.preco) AS total_gasto
+FROM vendas v
+INNER JOIN menu m ON v.produto_id = m.produto_id
+GROUP BY v.cliente_id
+ORDER BY total_gasto DESC;
+
+==================================================================
+2. Em quantos dias diferentes cada cliente visitou o restaurante?
+==================================================================
+
+SELECT 
+    cliente_id,
+	COUNT(DISTINCT data_pedido) AS dias_visitados
+FROM vendas
+GROUP BY cliente_id
+ORDER BY dias_visitados DESC;
+
+===================================================================
+3. Qual foi o primeiro item do cardápio comprado por cada cliente?
+===================================================================
+
+SELECT
+    v.cliente_id,
+    m.nome_produto,
+    v.data_pedido
+FROM vendas v
+JOIN menu m ON v.produto_id = m.produto_id
+WHERE v.data_pedido = (
+    SELECT MIN(data_pedido)
+    FROM vendas
+    WHERE cliente_id = v.cliente_id
+)
+ORDER BY v.cliente_id;
+
+O join aqui foi nescessário para trazer o nome do produto, que não consta na tabela vendas. 
+No WHERE v.data_pedido, pega a menor data do cliente.
+
+====================================================================================
+4. Qual é o item mais vendido do cardápio e quantas vezes ele foi comprado no total?
+====================================================================================
+
+
+5. Qual foi o item mais consumido por cada cliente?
+
+6. Qual item foi comprado primeiro por cada cliente após se tornar membro do programa de fidelidade?
+
+7. Qual item foi comprado imediatamente antes de o cliente se tornar membro?
+
+8. Qual a quantidade total de itens e o valor gasto por cada cliente antes de se tornar membro?
+
+9. Considerando que cada $1 gasto equivale a 10 pontos e que sushi possui multiplicador de 2x, quantos 
+pontos cada cliente acumulou?
+
+10. Durante a primeira semana após a adesão ao programa (incluindo a data de adesão), os clientes 
+ganham 2x pontos em todos os itens. Quantos pontos os clientes A e B acumularam até o final de janeiro?
